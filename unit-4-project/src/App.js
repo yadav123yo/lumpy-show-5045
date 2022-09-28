dcf24b8d23d8225986e1e2103880f8325f1d325f
Yogesh Yadav <yy072156@gmail.com>
2022-09-28 17:26:41 +0530

    done-2

---
+++ unit-4-project/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./Component/Footer";
+
+import Navbar from "./Component/Navbar";
+import SideMenu from "./Component/SideMenu";
+
+function App() {
+  return (
+    <>
+      <SideMenu />
+      <Navbar />
+      
+      <Footer />
+    </>
+  );
+}
+
+export default App;
