7617eed28e9e2c7f5e401c004c3d4761d4785092
Yogesh Yadav <yy072156@gmail.com>
2022-09-29 10:52:17 +0530

    day-3

--- unit-4-project/src/App.js
+++ unit-4-project/src/App.js
@@ -3,13 +3,14 @@ import Footer from "./Component/Footer";
 
 import Navbar from "./Component/Navbar";
 import SideMenu from "./Component/SideMenu";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
     <>
       <SideMenu />
       <Navbar />
-      
+      <AllRoutes />
       <Footer />
     </>
   );
